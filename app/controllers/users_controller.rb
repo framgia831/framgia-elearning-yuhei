8fd52f623a891cb2937b58954f5b9ed224b8863d
Yuhei Tsunoda <yuhei39.apollo@gmail.com>
2018-09-07 11:24:20 +0800

    signup_2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,19 +12,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(
-      name: params[:user][:name],
-      email: params[:user][:email],
-      password: params[:user][:password],
-      password_confirmation: params[:user][:password_confirmation]
-      )
-
+    @user = User.new(user_params)
     if @user.save
     redirect_to user_url(@user)
     else
       render 'new'
     end
-     
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
 	has_secure_password
 	validates :name, presence: true
-	validates :email, presence: true
 	validates :password, presence: true
+	
+	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true,
+					  format: { with: EMAIL_REGEX },
+					  uniqueness: true
 
 end

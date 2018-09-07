26e93b84d9033dd27fddb8fba20990c152fbd8f5
YuheiTsunoda <42732270+YuheiTsunoda@users.noreply.github.com>
2018-09-07 12:44:47 +0800

    signup (#3)
    * signup
    
    * signup_2
    
    * signup_3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 	root "static_pages#home"
- 	resources :users
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get "/signup", to: "users#new"
+	get "/login", to: "sessions#new"
+	post "/login", to: "sessions#create"
+	delete "/logout", to: "sessions#destroy"
+	
+ 	resources :users, except: :new
+ # For details the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
 end

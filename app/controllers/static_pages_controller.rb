7f98f4dcc08b05f58071a56e335216a2cfce4ed6
YuheiTsunoda <42732270+YuheiTsunoda@users.noreply.github.com>
2018-09-10 12:29:29 +0800

    Edit page, and image upload (#4)
    * edit
    
    * second check

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
 def home
-	
+	if current_user
+		render "users/dashboard"
+	end
 end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+    
 	end
 
 	def new
@@ -12,7 +13,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(
+      user_params
+      )
     if @user.save
     redirect_to user_url(@user)
     else
@@ -20,9 +23,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+
+    # @user.name = params[:name]
+    # @user.email = params[:email]
+    # @user.image = params[:image]
+
+    if @user.update_attributes(user_params)
+      # @user.save
+      flash[:notice] = "Success"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/edit")
+    end
+  end
+
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
     end
 
 end

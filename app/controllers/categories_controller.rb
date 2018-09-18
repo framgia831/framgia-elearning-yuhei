d55e22f4edd357ca0be69da336ec7d635494213e
Yuhei Tsunoda <yuhei39.apollo@gmail.com>
2018-09-18 11:31:01 +0800

    Create Lesson function4

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,15 +4,16 @@ class CategoriesController < ApplicationController
 		# users = User.where(id: current_user.id)
 		# lessons = Lesson.where(user_id: current_user.id)
 		# num = lessons.pluck(:category_id)
-		@categories_done = current_user.categories
+		# @categories_done = current_user.categories
 
-		@categories = @categories_all - @categories_done
+		@categories = @categories_all - current_user.categories
 	end
 
 	def show
-		@category = Category.find(params[:id])
-		lessons = @category.lessons
-		@lesson = lessons.find_by(user_id: current_user.id)
+		@lesson = current_user.lessons.find_by(category_id: params[:id])
+		# @category = Category.find(params[:id])
+		# lessons = @category.lessons
+		# @lesson = lessons.find_by(user_id: current_user.id)
 		@answers = @lesson.answers
 	end
 end

aa18bd1c7cb0fef0baf1809a0dc86dadf2d3790c
Yuhei Tsunoda <yuhei39.apollo@gmail.com>
2018-09-18 11:33:14 +0800

    Create Lesson function5

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,19 +1,7 @@
 class AnswersController < ApplicationController
 	def new
-		# @lesson = Lesson.create(user_id: current_user.id, category_id: params[:category_id])
-		# @category = Category.find(params[:category_id])
-		@lesson = Lesson.find(params[:lesson_id])
 		@category = @lesson.category
-		# @answer = @lesson.answers.build
-
-		# words_all = Word.where(category_id: @category.id)	
-
-		# @answers = @lesson.answers.where(lesson_id: @lessons.ids)
-		# num = answers.pluck(:lesson_id)
-		# words_done = Word.where(id: num)	
-
 		@word = (@category.words - @lesson.words).first 
-		# @choices = @word.choices	
 	end 
 
 	def index
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,11 @@
 class CategoriesController < ApplicationController
 	def index
 		@categories_all = Category.all
-		# users = User.where(id: current_user.id)
-		# lessons = Lesson.where(user_id: current_user.id)
-		# num = lessons.pluck(:category_id)
-		# @categories_done = current_user.categories
-
 		@categories = @categories_all - current_user.categories
 	end
 
 	def show
 		@lesson = current_user.lessons.find_by(category_id: params[:id])
-		# @category = Category.find(params[:id])
-		# lessons = @category.lessons
-		# @lesson = lessons.find_by(user_id: current_user.id)
 		@answers = @lesson.answers
 	end
 end

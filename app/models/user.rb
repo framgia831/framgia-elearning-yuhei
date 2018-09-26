723b452690a2b6a2ef820254d7a272955b8a25a5
Yuhei Tsunoda <yuhei39.apollo@gmail.com>
2018-09-26 12:27:52 +0800

    new relationship4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
 	has_many :answers, through: :lessons
 	has_many :words, through: :lessons
 
-	has_many :relationships, class_name:  "Active_relationship",
+	has_many :active_relationships, class_name:  "Active_relationship",
                              foreign_key: "follower_id",
                              dependent:   :destroy
 
- 	has_many :relationships, class_name:  "Passive_relationship",
+ 	has_many :passive_relationships, class_name:  "Passive_relationship",
                              foreign_key: "followed_id",
                              dependent:   :destroy
 
  	has_many :following, through: :active_relationships, source: :followed
- 	has_many :followers,through: :passive_relationships, source: :follower
+ 	has_many :followers, through: :passive_relationships, source: :follower
 
 
 

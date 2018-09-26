5be8168d3878fed1369a9adecfc94cc8f9f6e3d7
YuheiTsunoda <42732270+YuheiTsunoda@users.noreply.github.com>
2018-09-26 11:34:07 +0800

    for rerquest (#13)
    * for rerquest
    
    * for request2
    
    * for request3

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,12 +6,12 @@ class RelationshipsController < ApplicationController
 			 )
 		relationship.create_activity(user_id: current_user.id)
 
-		redirect_to user_url(params[:followed_id])
+		redirect_to request.referrer || user_url(params[:followed_id]) 
 	end
 
 	def destroy
 		relationship = Relationship.find(params[:id])		
 		relationship.destroy
-		redirect_to user_url(relationship.followed_id)
+		redirect_to request.referrer || user_url(relationship.followed_id)
 	end
 end

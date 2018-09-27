e0743197fbc1f88b31f33d3e0f3f736b5f6033ce
Yuhei Tsunoda <yuhei39.apollo@gmail.com>
2018-09-27 11:36:04 +0800

    filter function1

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,18 @@
 class CategoriesController < ApplicationController
 	def index
 		@categories_all = Category.all.page(params[:page]).per(4)
-		@categories = @categories_all - current_user.categories
+	
+		if params[:learned] == 1.to_s
+			@categories = current_user.categories
+		elsif params[:learned] == 0.to_s
+			@categories = @categories_all - current_user.categories
+		else
+			@categories = @categories_all
+		end	
+				
+			
+
+
 	end
 
 	def show

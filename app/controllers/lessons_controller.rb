d4fd8908dda6482f04c5e7c9e5afe50aa437dfdc
Yuhei Tsunoda <yuhei39.apollo@gmail.com>
2018-09-28 12:21:41 +0800

    fix filter1

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,12 +8,13 @@ class LessonsController < ApplicationController
 	end	
 
 	def word
-			@category_id = params[:category_id]
+		@category_id = params[:category_id]
 		if @category_id == ""
-			@answers = current_user.answers.page(params[:page]).per(7)
+			@answers = current_user.answers
 		else
 			@answers = Answer.joins(:lesson).where("lessons.category.id": params[:category_id], "lessons.user_id": current_user.id)
 		end
+		@answers = @answers.page(params[:page]).per(7)
 	end
 
 	
